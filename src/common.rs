1763b1d8c93f61acc0783bf87bfabf8ca37f303d
aylah5 <528905223@qq.com>
2025-06-03 10:47:51 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://sec002.8866.org:21114".to_owned()
 }
 
 #[inline]
